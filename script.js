da47ebe3f021ebabfa87867409953aec32e6834d
CoenAntens <jacobusantens@gmail.com>
2023-08-04 08:29:26 +0200

    Made canvas bigger

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function canvas_read_mouse(canvas, e) {
     */
 
     let canvasRect = canvas.getBoundingClientRect();
-    
+
     canvas.tc_x1 = canvas.tc_x2;
     canvas.tc_y1 = canvas.tc_y2;
     canvas.tc_x2 = e.offsetX;
@@ -67,8 +67,14 @@ function canvas_read_touch(canvas, e) {
 
     canvas.tc_x1 = canvas.tc_x2;
     canvas.tc_y1 = canvas.tc_y2;
+
+    canvas.tc_x2 = e.clientX - canvasRect.left;
+    canvas.tc_y2 = e.clientY - canvasRect.top;
+
+    /*
     canvas.tc_x2 = touch.pageX - document.documentElement.scrollLeft - canvasRect.left;
     canvas.tc_y2 = touch.pageY - document.documentElement.scrollTop - canvasRect.top;
+    */
 }
 
 function on_canvas_touch_start(e) {
