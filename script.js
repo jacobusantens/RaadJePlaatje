34026cf3aff9ac2adb07440f165d28ffc0de9369
CoenAntens <jacobusantens@gmail.com>
2023-08-23 08:47:13 +0200

    Consulting the manual

--- script.js
+++ script.js
@@ -55,12 +55,17 @@ function canvas_read_touch(canvas, e) {
     
     let canvasRect = canvas.getBoundingClientRect();
 
-    let touch = event.touches[0];
-    
+    // let touch = event.touches[0];
+
+    // let touch = event.touches[0];
+
+    var x = e.touches[0].clientX;
+    var y = e.touches[0].clientY;
+
     canvas.tc_x1 = canvas.tc_x2;
     canvas.tc_y1 = canvas.tc_y2;
-    canvas.tc_x2 = touch.pageX - document.documentElement.scrollLeft - canvasRect.left;
-    canvas.tc_y2 = touch.pageY - document.documentElement.scrollTop - canvasRect.top;
+    canvas.tc_x2 = x - canvasRect.left;
+    canvas.tc_y2 = y - canvasRect.top;
 
     /*
     
