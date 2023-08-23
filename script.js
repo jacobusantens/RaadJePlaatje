24c893375beeb190de2951f615299bceb68618f7
CoenAntens <jacobusantens@gmail.com>
2023-08-23 08:58:36 +0200

    Keep on simplifying

--- script.js
+++ script.js
@@ -55,17 +55,14 @@ function canvas_read_touch(canvas, e) {
     
     let canvasRect = canvas.getBoundingClientRect();
 
-    // let touch = event.touches[0];
 
-    // let touch = event.touches[0];
-
-    var x = e.touches[0].offsetX;
-    var y = e.touches[0].offsetY;
+    // var x = e.touches[0].offsetX;
+    // var y = e.touches[0].offsetY;
 
     canvas.tc_x1 = canvas.tc_x2;
     canvas.tc_y1 = canvas.tc_y2;
-    canvas.tc_x2 = x;
-    canvas.tc_y2 = y;
+    canvas.tc_x2 = e.touches[0].offsetX;
+    canvas.tc_y2 = e.touches[0].offsetY;
 
     /*
     
@@ -151,7 +148,7 @@ function on_canvas_touch_move(e) {
 
 function drawLine(context, x1, y1, x2, y2) {
   context.beginPath();
-  context.strokeStyle = 'blue';
+  context.strokeStyle = 'green';
   context.lineWidth = 13;
   context.lineJoin = "round";
   context.moveTo(x1, y1);
